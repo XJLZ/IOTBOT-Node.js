1fc59ba1012fcddae67b71240f92ae766d9e6abe
XIAO <xiaonokia_no1@qq.com>
2020-07-13 08:31:51 +0800

    update

--- Plugins/Mongodb/db.js
+++ Plugins/Mongodb/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const fs = require('fs')
 const config = JSON.parse(fs.readFileSync('./config.json'))
 const mongodbUrl = config.MONGODBURL
 mongoose.connect(mongodbUrl, {
